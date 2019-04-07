3b351c4898998dbe3967a1dc9284714466d3d8b3
Marenthyu <petersilie93@live.de>
2019-04-07 14:31:05 +0200

    Initial hub-specific changes and IDE cleanup Only use WSS, as we only serve over HTTPS

--- resources/hubstats.js
+++ resources/hubstats.js
@@ -1,17 +1,17 @@
 function roomNames(item, index, arr){
-  let node = document.createElement("li")
-  let kbd = document.createElement("kbd")
-  let name = document.createTextNode(item.RoomName)
-  let link = document.createElement("a")
+  let node = document.createElement("li");
+  let kbd = document.createElement("kbd");
+  let name = document.createTextNode(item.RoomName);
+  let link = document.createElement("a");
 
-  node.setAttribute('data-path', item.Path)
-  node.setAttribute('class', 'room')
-  node.setAttribute('data-name', item.RoomName)
-  link.setAttribute('href', '#' + item.Path)
+  node.setAttribute('data-path', item.Path);
+  node.setAttribute('class', 'room');
+  node.setAttribute('data-name', item.RoomName);
+  link.setAttribute('href', '#' + item.Path);
 
-  kbd.appendChild(name)
-  link.appendChild(kbd)
-  node.appendChild(link)
+  kbd.appendChild(name);
+  link.appendChild(kbd);
+  node.appendChild(link);
   roomsObject.appendChild(node)
 }
 
@@ -19,33 +19,34 @@ function setRoomListeners(roomSocket, roomName){
   // Connection opened
   roomSocket.onopen = function(event) {
     //console.log("Room WebSocket is open now. (" + roomName + ")")
-  }
+  };
   
   // Log errors
   roomSocket.onerror = function(event) {
     console.error("Room WebSocket error observed:", event)
-  }
+  };
 
   // Connection closed
   roomSocket.onclose = function(event) {
     //console.log("Room WebSocket is now closed. (" + roomName + ")")
-    tableObject.innerHTML = ''
+    tableObject.innerHTML = '';
     statusObject.innerHTML = ''
-  }
+  };
 
   // Listen for messages 
   roomSocket.onmessage = function(event) {
-    let message = JSON.parse(event.data)
-    if (message.commandType == "GetRoomInfo"){
+    let message = JSON.parse(event.data);
+    let roomState;
+    if (message.commandType === "GetRoomInfo") {
       //let maxPlayers = message.data.maxPlayers
       //let name = message.data.name
       //let noFail = message.data.noFail
       //let roomHost = message.data.roomHost
       //let roomId = message.data.roomId
-      roomState = message.data.roomState
+      roomState = message.data.roomState;
       playersNum = message.data.players;
       //let selectedDifficulty = message.data.selectedDifficulty
-      if(message.data.selectedSong){
+      if (message.data.selectedSong) {
         songName = message.data.selectedSong.songName
         //let levelId = message.data.selectedSong.levelId
         //let songDuration = message.data.selectedSong.songDuration
@@ -60,12 +61,12 @@ function setRoomListeners(roomSocket, roomName){
         passwordState = ''
       }
     }
-    if (message.commandType == "PlayerReady"){
-      readyPlayers = message.data.readyPlayers
-      totalPlayers = message.data.roomClients
+    if (message.commandType === "PlayerReady"){
+      readyPlayers = message.data.readyPlayers;
+      totalPlayers = message.data.roomClients;
       roomState = 1
     }
-    if (message.commandType == "SetSelectedSong"){
+    if (message.commandType === "SetSelectedSong"){
       //let levelID = message.data.levelId
       //let songDuration = message.data.songDuration
       if (message.data){
@@ -74,52 +75,52 @@ function setRoomListeners(roomSocket, roomName){
         songName = "Selecting Song..."
       }
     }
-    if (message.commandType == "StartLevel"){
+    if (message.commandType === "StartLevel"){
       //let difficulty = message.data.difficulty
       //let levelID = message.data.song.levelId
       //let songDuration = message.data.song.songDuration
-      songName = message.data.song.songName
+      songName = message.data.song.songName;
       roomState = 2
     }
-    if (message.commandType == "UpdatePlayerInfo"){
-      let players = message.data
-      tableObject.innerHTML=''
-      playersNum = players.length
+    if (message.commandType === "UpdatePlayerInfo"){
+      let players = message.data;
+      tableObject.innerHTML='';
+      playersNum = players.length;
 
-      let tr = document.createElement('tr')
-      tr.innerHTML='<th>Name</th><th>Score</th><th>Combo</th><th>Energy</th>'
-      tableObject.appendChild(tr)
+      let tr = document.createElement('tr');
+      tr.innerHTML='<th>Name</th><th>Score</th><th>Combo</th><th>Energy</th>';
+      tableObject.appendChild(tr);
       players.forEach(
         function (element){
           //let playerAvatar = element.playerAvatar
-          let playerComboBlocks = element.playerComboBlocks
+          let playerComboBlocks = element.playerComboBlocks;
           //let playerCutBlocks = element.playerCutBlocks
-          let playerEnergy = element.playerEnergy
+          let playerEnergy = element.playerEnergy;
           //let playerId = element.playerId
-          let playerName = element.playerName
+          let playerName = element.playerName;
           //let playerProgress = element.playerProgress
-          let playerScore = element.playerScore
+          let playerScore = element.playerScore;
           //let playerState = element.playerState
 
-          let tr = document.createElement('tr')
-          let Name = document.createElement('td')
-          let Score = document.createElement('td')
-          let Combo = document.createElement('td')
-          let Energy = document.createElement('td')
+          let tr = document.createElement('tr');
+          let Name = document.createElement('td');
+          let Score = document.createElement('td');
+          let Combo = document.createElement('td');
+          let Energy = document.createElement('td');
 
-          Name.innerHTML=playerName
-          tr.appendChild(Name)
-          Score.innerHTML=playerScore
-          tr.appendChild(Score)
-          Combo.innerHTML=playerComboBlocks
-          tr.appendChild(Combo)
-          Energy.innerHTML=playerEnergy
-          tr.appendChild(Energy)
+          Name.innerHTML=playerName;
+          tr.appendChild(Name);
+          Score.innerHTML=playerScore;
+          tr.appendChild(Score);
+          Combo.innerHTML=playerComboBlocks;
+          tr.appendChild(Combo);
+          Energy.innerHTML=playerEnergy;
+          tr.appendChild(Energy);
           tableObject.appendChild(tr)
         }
       )
     } 
-    if (roomState == 1){
+    if (roomState === 1){
       playerNumber = "(" + readyPlayers + " / " + totalPlayers + ") players " + passwordState
     } else {
       playerNumber = playersNum + " players " + passwordState
@@ -132,48 +133,44 @@ function setRoomListeners(roomSocket, roomName){
 }
 
 function openRoom(element){
-  let roomName = element.getAttribute('data-name')
-  let roomPath = element.getAttribute('data-path')
+  let roomName = element.getAttribute('data-name');
+  let roomPath = element.getAttribute('data-path');
   // Create WebSocket connection.
   if (typeof roomSocket !== 'undefined') {
     roomSocket.close()
   }
-  if (window.location.protocol == "https:"){
-    roomSocket = new WebSocket('wss://hub.assistant.moe:3900' + roomPath)
+    roomSocket = new WebSocket('wss://waifus.de:3701' + roomPath);
     setRoomListeners(roomSocket, roomName)
-  } else {
-    roomSocket = new WebSocket('ws://hub.assistant.moe:3800' + roomPath)
-    setRoomListeners(roomSocket, roomName)
-  }
+
 }
 
 function setListeners(socket){
   // Connection opened
   socket.onopen = function(event) {
     //console.log("WebSocket is open now.")
-  }
+  };
   
   // Log errors
   socket.onerror = function(event) {
     console.error("WebSocket error observed:", event)
-  }
+  };
 
   // Listen for messages 
   socket.onmessage = function(event) {
-    let hub = JSON.parse(event.data)
+    let hub = JSON.parse(event.data);
     if (hub.Version) {
-      version = hub.Version
-      TotalClients = hub.TotalClients
+      version = hub.Version;
+      TotalClients = hub.TotalClients;
       rooms = hub.Rooms
     } else {
       rooms = hub
     }
-    let roomNumber = rooms.length
-    document.getElementById("roomNumber").innerHTML=roomNumber
-    document.getElementById("version").innerHTML="v " + version
-    document.getElementById("totalPlayers").innerHTML=TotalClients
-    roomsObject.innerHTML=''
-    rooms.forEach(roomNames)
+    let roomNumber = rooms.length;
+    document.getElementById("roomNumber").innerHTML=roomNumber;
+    document.getElementById("version").innerHTML="v " + version;
+    document.getElementById("totalPlayers").innerHTML=TotalClients;
+    roomsObject.innerHTML='';
+    rooms.forEach(roomNames);
     Array.from(roomsObject.children).forEach(
       function (element){
         element.addEventListener("click", function () {
@@ -184,25 +181,20 @@ function setListeners(socket){
   }
 }
 
-let roomSocket
-let rooms = []
-let roomStatus
-let readyPlayers
-let totalPlayers
-let playersNum
-let playerNumber = ''
-let songName
-let version
-let TotalClients
-const tableObject = document.getElementById("players")
-const statusObject = document.getElementById("roomStatus")
-const roomsObject = document.getElementById("rooms")
+let roomSocket;
+let rooms = [];
+let roomStatus;
+let readyPlayers;
+let totalPlayers;
+let playersNum;
+let playerNumber = '';
+let songName;
+let version;
+let TotalClients;
+const tableObject = document.getElementById("players");
+const statusObject = document.getElementById("roomStatus");
+const roomsObject = document.getElementById("rooms");
 
 // Create WebSocket connection.
-if (window.location.protocol == "https:"){
-  let socket = new WebSocket('wss://hub.assistant.moe:3900/')
-  setListeners(socket)
-} else {
-  let socket = new WebSocket('ws://hub.assistant.moe:3800/')
-  setListeners(socket)
-}
+let socket = new WebSocket('wss://waifus.de:3701/');
+setListeners(socket);
